45fc1008ed8c1b639e01db2b6e7d615241b91d61
panish <panishakoliya22@gmail.com>
2022-04-28 09:55:36 +0530

    sidebar changes

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -8,44 +8,15 @@
 
     <div class="app-menu">
         <ul class="accordion-menu">
-
-            @can('dashboard')
-                <li class="@if(Route::is('dashboard'))active-page @endif">
-                    <a href="{{route('dashboard')}}" class="active"><i class="material-icons-two-tone">dashboard</i>Dashboard</a>
-                </li>
-            @endcan
-
-            @can('view permission')
-                <li class="@if(Route::is('role.*'))active-page @endif">
-                    <a href="{{route('role.index')}}" class="active"><i class="material-icons-two-tone">shield</i>Roles</a>
-                </li>
-            @endcan
-
-            @can('view permission')
-                <li class="@if(Route::is('permission.*'))active-page @endif">
-                    <a href="{{route('permission.index')}}" class="active"><i
-                            class="material-icons-two-tone">shield</i>Permissions</a>
-                </li>
-            @endcan
-
-            @can('view user')
-                <li class="@if(Route::is('user.*')) active-page @endif">
-                    <a href="{{route('user.index')}}"
-                       class="@if(Route::is('user.index')) active @endif">
-                        <i class="material-icons-two-tone">person</i>Users
-                    </a>
-                </li>
-            @endcan
-
-            @can('view salon')
-                <li class="@if(Route::is('salon.*')) active-page @endif">
-                    <a href="{{route('salon.index')}}"
-                       class="@if(Route::is('salon.index')) active @endif">
-                        <i class="material-icons-two-tone">view_sidebar</i>Salons
-                    </a>
-                </li>
-            @endcan
-
+            <li class="@if(Route::is('dashboard'))active-page @endif">
+                <a href="{{route('dashboard')}}" class="active"><i class="material-icons-two-tone">dashboard</i>Dashboard</a>
+            </li>
+            <li class="@if(Route::is('user.*')) active-page @endif">
+                <a href="{{route('user.index')}}"
+                   class="@if(Route::is('user.index')) active @endif">
+                    <i class="material-icons-two-tone">person</i>Users
+                </a>
+            </li>
         </ul>
     </div>
 </div>
